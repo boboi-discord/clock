fb9f03afdb659932e0dea8bf96b86da7bfe24dfe
boboi-discord <66130809+boboi-discord@users.noreply.github.com>
2020-05-30 01:46:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     while True:
         now = datetime.datetime.now()
-        await client.get_channel(timechannel).edit(name=f"{now.hour}:{now.minute} (<+8>)") # The channel gets changed here
+        await client.get_channel(timechannel).edit(name=f"{now.hour}:{now.minute} (+8) # The channel gets changed here
         await asyncio.sleep(60)
 
 
